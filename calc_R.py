0abef7947f744048c9658e9bbfaa4e9140e1f1a0
Woldeamanuel <emanuel.birru@gmail.com>
2014-12-11 21:48:15 +1100

    Multiply fun included

--- calc_R.py
+++ calc_R.py
@@ -1,10 +1,12 @@
 import sys
 def add_all(nums):
     return sum(nums)
-if __name__ == '__main__':
-    print(sum(map(float, sys.argv[1:])))
+def multiply_all(num):
+    return reduce(lambda a, b: a * b, nums)
 if __name__ == '__main__':
     command = sys.argv[1]
     nums = map(float, sys.argv[2:])
     if command == 'add':
         print(add_all(nums))
+    elif command == 'multiply':
+        print(multiply_all(nums))
